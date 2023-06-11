cccc84a2906620c5cf29b479d5eb8ce510d656da
Dr Stephen Lynch FIMA SFHEA <59564808+proflynch@users.noreply.github.com>
2023-06-11 07:42:15 +0100

    Add files via upload

--- IDLE Files/cantor.py
+++ IDLE Files/cantor.py
@@ -1,16 +1,17 @@
-# Cantor Set
+# Cantor Fractal.
+# Run Module and type >>> cantor(-400 , 200 , 729)
 from turtle import *
-def cantor3(x , y , length):
-        speed(0)
-        if length >= 5:
+def cantor(x , y , length):
+        speed(0) # Fastest speed.
+        if length >= 5: # Stop when length < 5.
             penup()
             pensize(2)
             pencolor("blue")
             setpos(x , y)
             pendown()
             fd(length)
-            y -= 50
-            cantor3(x , y , length / 3)
-            cantor3(x + 2 * length / 3 , y , length / 3)
+            y -= 60 # Height difference.
+            cantor(x , y , length / 3)
+            cantor(x + 2 * length / 3 , y , length / 3)
             penup()
             setpos(x , y + 50)
